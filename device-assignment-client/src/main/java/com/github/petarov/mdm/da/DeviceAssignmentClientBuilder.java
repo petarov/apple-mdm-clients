f1c88b71beb58149c515db0581e9514debbdfaac
Petar Petrov <ppetrov@vexelon.net>
2025-02-09 21:03:03 +0100

    Fix builder defaults

--- device-assignment-client/src/main/java/com/github/petarov/mdm/da/DeviceAssignmentClientBuilder.java
+++ device-assignment-client/src/main/java/com/github/petarov/mdm/da/DeviceAssignmentClientBuilder.java
@@ -5,11 +5,16 @@ import com.github.petarov.mdm.shared.config.MdmClientBuilder;
 import com.github.petarov.mdm.shared.http.MdmHttpClient;
 import jakarta.annotation.Nonnull;
 
+import java.time.Duration;
+import java.util.Objects;
+
 public class DeviceAssignmentClientBuilder
 		extends MdmClientBuilder<DeviceAssignmentClientBuilder, DeviceAssignmentClient> {
 
-	private final String DEFAULT_APPLE_PRODUCTION_HOST = "https://mdmenrollment.apple.com";
-	private final String DEFAULT_USER_AGENT            = "apple-mdm-clients-v1";
+	private final String   DEFAULT_APPLE_PRODUCTION_HOST = "https://mdmenrollment.apple.com";
+	private final String   DEFAULT_USER_AGENT            = "apple-mdm-clients-v1";
+	private final Duration DEFAULT_CONNECT_TIMEOUT       = Duration.ofSeconds(10);
+	private final Duration DEFAULT_READ_TIMEOUT          = Duration.ofSeconds(20);
 
 	private String                      appleHost;
 	private DeviceAssignmentServerToken serverToken;
@@ -34,6 +39,11 @@ public class DeviceAssignmentClientBuilder
 	@Nonnull
 	@Override
 	public DeviceAssignmentClient build() {
+		setServiceUrl(Objects.requireNonNullElse(serviceUrl, DEFAULT_APPLE_PRODUCTION_HOST));
+		setUserAgent(Objects.requireNonNullElse(userAgent, DEFAULT_USER_AGENT));
+		setConnectTimeout(Objects.requireNonNullElse(connectTimeout, DEFAULT_CONNECT_TIMEOUT));
+		setReadTimeout(Objects.requireNonNullElse(readTimeout, DEFAULT_READ_TIMEOUT));
+
 		return new DeviceAssignmentClient(new MdmHttpClient(
 				new MdmBuilderOptions(serviceUrl, userAgent, skipSslVerify, connectTimeout, readTimeout, proxyOptions)),
 				serverToken);
