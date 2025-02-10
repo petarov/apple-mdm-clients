13b5019b2d1eddbd1ce864cdcbca908c54634e2e
Petar Petrov <ppetrov@vexelon.net>
2025-02-10 13:31:17 +0100

    Add json util unit tests

--- shared/src/main/java/com/github/petarov/mdm/shared/util/JsonUtil.java
+++ shared/src/main/java/com/github/petarov/mdm/shared/util/JsonUtil.java
@@ -2,7 +2,7 @@ package com.github.petarov.mdm.shared.util;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.annotation.Nonnull;
 
@@ -15,8 +15,10 @@ public final class JsonUtil {
 	public static ObjectMapper createObjectMapper() {
 		var mapper = new ObjectMapper();
 		mapper.registerModule(new JavaTimeModule());
-		mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-		// Do not convert to local timezone
+		//		mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+		// Format date-time as ISO-8601
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		// Do not convert date-time to local timezone
 		mapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
 		return mapper;
 	}
