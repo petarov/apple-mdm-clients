ee1c843470addcca72103c51f1069ed1d0108c7a
Petar Petrov <ppetrov@vexelon.net>
2025-03-05 15:38:45 +0100

    Make LegacyAppAndBookClientBuilder part of the client interface

--- legacy-app-and-book-management-client/src/main/java/com/github/petarov/mdm/aab/legacy/LegacyAppAndBookClient.java
+++ legacy-app-and-book-management-client/src/main/java/com/github/petarov/mdm/aab/legacy/LegacyAppAndBookClient.java
@@ -1,3 +1,14 @@
 package com.github.petarov.mdm.aab.legacy;
 
-public interface LegacyAppAndBookClient {}
+import jakarta.annotation.Nonnull;
+
+public interface LegacyAppAndBookClient {
+
+	/**
+	 * @return new {@link LegacyAppAndBookClientBuilder} instance
+	 */
+	@Nonnull
+	static LegacyAppAndBookClientBuilder newBuilder() {
+		return new LegacyAppAndBookClientBuilder();
+	}
+}
--- legacy-app-and-book-management-client/src/main/java/com/github/petarov/mdm/aab/legacy/LegacyAppAndBookClientBuilder.java
+++ legacy-app-and-book-management-client/src/main/java/com/github/petarov/mdm/aab/legacy/LegacyAppAndBookClientBuilder.java
@@ -9,6 +9,9 @@ public class LegacyAppAndBookClientBuilder
 	private final String DEFAULT_APPLE_PRODUCTION_HOST = "https://vpp.itunes.apple.com/mdm";
 	private final String DEFAULT_USER_AGENT            = "apple-mdm-clients-v1";
 
+	LegacyAppAndBookClientBuilder() {
+	}
+
 	@Nonnull
 	@Override
 	public LegacyAppAndBookClient build() {
